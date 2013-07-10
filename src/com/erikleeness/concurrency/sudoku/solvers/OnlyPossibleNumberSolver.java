81eacd484ae5db3473ccea541c3bd89712cd9a0e
Erik Ness <erikness@nmsu.edu>
2013-07-09 21:20:45 -0600

    Draft of OnlyPossibleNumberSolver which, given some shared code with SimpleEliminationSolver, turned out to be pretty short.

--- src/com/erikleeness/concurrency/sudoku/solvers/OnlyPossibleNumberSolver.java
+++ src/com/erikleeness/concurrency/sudoku/solvers/OnlyPossibleNumberSolver.java
@@ -1,16 +1,39 @@
 package com.erikleeness.concurrency.sudoku.solvers;
 
+import java.util.Set;
+
 import com.erikleeness.concurrency.sudoku.Pair;
 import com.erikleeness.concurrency.sudoku.Sudoku;
+import com.erikleeness.concurrency.sudoku.Sudokus;
+import com.google.common.collect.Sets;
 
-public class OnlyPossibleNumberSolver extends Solver {
-
-	public OnlyPossibleNumberSolver(Solver nextSolver) {
+public class OnlyPossibleNumberSolver extends Solver
+{
+	private Sudoku workingPuzzle;
+	
+	public OnlyPossibleNumberSolver(Solver nextSolver)
+	{
 		super(nextSolver);
 	}
 
 	@Override
-	public boolean confirmSolutionExistsFor(Sudoku puzzle, Pair<Integer, Integer> targetLocation) {
+	public boolean confirmSolutionExistsFor(Sudoku puzzle, Pair<Integer, Integer> targetLocation)
+	{
+		workingPuzzle = puzzle.copy();
+		
+		Set<Integer> conflictsWithTarget = Sets.newHashSet();
+		
+		for (Integer n : Sudokus.intSeq(1, 9)) {
+			if (Sudokus.directConflictExistsBetween( n, targetLocation, workingPuzzle )) {
+				conflictsWithTarget.add(n);
+			}
+		}
+		
+		if (conflictsWithTarget.size() == 8) {
+			// There's only one it could possibly be!
+			return true;
+		}
+		
 		return nextSolver.confirmSolutionExistsFor(puzzle, targetLocation);
 	}
 
